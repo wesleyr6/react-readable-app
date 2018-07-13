ca0c3032bec280ab1d8e50d1325aa38a41d0f6dc
Wesley Amaro <contato@wesleyamaro.com>
2018-07-12 21:13:50 -0300

    type: feat creating actions to add comments

--- client/src/actions/comments.js
+++ client/src/actions/comments.js
@@ -1,6 +1,7 @@
 import { APIResquest } from '../utils/API';
 
 export const GET_COMMENTS = 'GET_COMMENTS';
+export const SAVE_COMMENTS = 'SAVE_COMMENTS';
 
 export function loadComments(postID) {
 	return dispatch => {
@@ -9,6 +10,19 @@ export function loadComments(postID) {
 			method: 'GET'
 		}).then(res => {
 			dispatch({ type: GET_COMMENTS, payload: res });
+		});
+	};
+}
+
+export function addComments(data) {
+	return dispatch => {
+		APIResquest({
+			uri: 'comments',
+			method: 'POST',
+			data
+		}).then(res => {
+			dispatch({ type: SAVE_COMMENTS, payload: res });
+			dispatch({ type: GET_COMMENTS, payload: [res] });
 		});
 	};
 }
