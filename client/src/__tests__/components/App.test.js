fe9e639e815cf5300242beb11aabc3b2ced3b6da
Wesley Amaro <contato@wesleyamaro.com>
2018-07-23 22:01:45 -0300

    type: feat adding jest configs

---
+++ client/src/__tests__/components/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import { shallow } from 'enzyme';
+//import App from '../../components/App/';
+
+it('App', () => {
+	//const wrapper = shallow(<App/>);
+	const main = <main />;
+	expect(true).toEqual(true);
+});
