0b264e07b991cb5cdb0183aa9033f61010bf792e
Wesley Amaro <contato@wesleyamaro.com>
2018-07-28 22:34:53 -0300

    type: test

--- client/src/__tests__/pages/Category.test.js
+++ client/src/__tests__/pages/Category.test.js
@@ -18,22 +18,35 @@ describe('Category Page', () => {
 		wrapper = shallow(<CategoryPage
 			loadPostsByCategory={initialState.mockFn}
 			posts={initialState.posts}
-			match={initialState.match} />);
+			match={initialState.match} />, { lifecycleExperimental: true });
 	});
 
 	afterEach(() => {
 		initialState.mockFn.mockReset();
+		initialState.mockFn.mockRestore();
 	});
 
 	it('Should render component', () => {
 		const { mockFn } = initialState;
 
 		expect(wrapper).toHaveLength(1);
+		expect(wrapper.find('h1').html()).toMatch(/react/);
 		expect(mockFn).toHaveBeenCalled();
 		expect(mockFn.mock.calls).toHaveLength(1);
 	});
 
-	it('Should contains the category into <h1 />', () => {
-		expect(wrapper.find('h1').html()).toMatch(/react/);
+	it('Should render component with new data on method ComponentDidMount', () => {
+		const { mockFn } = initialState;
+
+		wrapper.setProps({
+			match: {
+				params: {
+					category: 'redux'
+				}
+			}
+		});
+
+		expect(wrapper.find('h1').html()).toMatch(/redux/);
+		expect(mockFn).toHaveBeenCalled();
 	});
 });
