76fd619e552b52d094e4d19d3b3f5b514c1051e2
Wesley Amaro <contato@wesleyamaro.com>
2018-07-30 19:49:18 -0300

    type: fix

--- server/index.js
+++ server/index.js
@@ -313,6 +313,16 @@ app.delete('/comments/:id', (req, res) => {
 		);
 });
 
+
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, '../client/build')));
+	// Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+	});
+}
+
 app.listen(config.port, () => {
 	console.log('Server listening on port %s, Ctrl+C to stop', config.port);
 });
