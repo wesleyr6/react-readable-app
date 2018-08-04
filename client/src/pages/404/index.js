3215e14878bb6d0157ce29921476502201d5d922
Wesley Amaro <contato@wesleyamaro.com>
2018-08-04 15:18:46 -0300

    type: feat adding alerts component

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Alerts from '../../components/Alerts/';
 
 const NotFoundPage = () => {
 	return(
 		<div className="wrapper">
 			<h1>404 - Page not found</h1>
-			<p>We can not seem to find the page you are looking for.</p>
+			<Alerts type="error" message="We can not seem to find the page you are looking for." />
 			<Link className="button blue-theme margin-top" to="/">Go back to Home</Link>
 		</div>
 	);
